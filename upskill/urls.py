1a71c12bd2b6d3484ff2114d5a1a09e86b47bded
diaz <rahimovshohjahon69@gmail.com>
2025-12-17 16:46:52 +0500

    Courses

--- upskill/urls.py
+++ upskill/urls.py
@@ -1,14 +1,11 @@
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
-from .views import course_list, index, course_detail
-from . import views 
+from .views import CourseListView, IndexView, CourseDetailView
+
+
 urlpatterns = [
-    path('', index, name='index'),
-    path('courses/', course_list, name='course_list'),
-    path('courses/<int:pk>/', course_detail, name='course_detail'),
-    path('', views.index, name='index'),
-    path('courses/', views.course_list, name='course_list'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('', IndexView.as_view(), name='index'),
+    path('courses/', CourseListView.as_view(), name='course_list'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- upskill/views.py
+++ upskill/views.py
@@ -1,44 +1,53 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, TemplateView, DetailView
 from .models import Course
-# Create your views here.
 
-# from django.shortcuts import render, get_object_or_404
-# from django.views.generic import ListView, TemplateView, DetailView
-# from .models import Course
-
-# class CourseListView(ListView):
-#     model = Course
-#     template_name = 'course.html'
-#     context_object_name = 'courses'
-
-# class IndexView(TemplateView):
-#     template_name = 'index.html'
-
-# class CourseDetailView(DetailView):
-#     model = Course
-#     template_name = 'detail.html'
-#     context_object_name = 'course'
+class CourseListView(ListView):
+    model = Course
+    template_name = 'course.html'
+    context_object_name = 'courses'
 
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-def index(request):
-    return render(request, 'index.html')
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'detail.html' 
+    context_object_name = 'course'
 
-def course_list(request):
-    courses = Course.objects.select_related('owner', 'subject').all()
-    return render(request, 'course.html', {'courses': courses})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['related_courses'] = Course.objects.exclude(pk=self.object.pk)[:3]
+        return context
     
 
-def course_detail(request, pk):
-    course =  get_object_or_404(Course, pk = pk)
-    return render(request, 'detail.html', {'course': course})
 
-def home(request):
-    return render(request, 'home.html')
+# from django.shortcuts import render, get_object_or_404
+# from .models import Course
+# # Create your views here.
 
-def about(request):
-    return render(request, 'about.html')
 
-def contact(request):
-    return render(request, 'contact.html')
+# def index(request):
+#     return render(request, 'index.html')
+
+
+
+# def course_list(request):
+#     courses = Course.objects.select_related('owner', 'subject').all()
+#     return render(request, 'course.html', {'courses': courses})
+    
+
+# def course_detail(request, pk):
+#     course =  get_object_or_404(Course, pk = pk)
+#     return render(request, 'detail.html', {'course': course})
+
+# def home(request):
+#     return render(request, 'home.html')
+
+# def about(request):
+#     return render(request, 'about.html')
+
+# def contact(request):
+#     return render(request, 'contact.html')
 
